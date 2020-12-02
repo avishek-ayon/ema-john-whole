89c7cbd7d716da07a0f853c34c4d8a84be8bc7bd
avishek-ayon <avishek.ayon96@gmail.com>
2020-12-02 12:29:40 +0600

    one

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -2,25 +2,32 @@ import React from 'react';
 
 const ReviewItem = (props) => {
     console.log(props);
-    const{name,quantity,key,price }=props.product;
+    const{name,quantity,key,price,img }=props.product;
     const reviewItemStyle=
         {marginBottom:'5px',
         borderBottom:'1px solid lightgray',
         paddingBottom:'5px',
-        marginLeft:'200px'
+        display:'flex'
+        
         
     };
     return (
 
         <div style={reviewItemStyle} className="review-item">
-            <h3>{name}</h3>
-            <p>Quantity:{quantity}</p>
-            <p><small>${price}</small></p>
-            <br/>
-            <button 
-            className="main-button"
-            onClick={()=>props.removeProduct(key)}
-            >Remove</button>
+            <div>
+                <img src={img} alt=""/>
+            </div>
+            <div>
+                <h3>{name}</h3>
+                <p>Quantity:{quantity}</p>
+                <p><small>${price}</small></p>
+                <br/>
+                <button 
+                className="main-button"
+                onClick={()=>props.removeProduct(key)}
+                >Remove</button>
+            </div>
+           
         </div>
     );
 };
